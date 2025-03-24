a56e1242b1ce72b8bd74124de37783904e8b7872
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2025-03-24 18:22:24 +0100

    feat: event listener on document

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const $one = document.querySelector.bind(document);
 const $all = document.querySelectorAll.bind(document);
 
+const searchBar = $one('#search-bar');
+
+document.addEventListener('keypress', (event) => {
+
+    if (event.key === 'Enter') {
+        task = searchBar.value;
+        console.log(task);
+    }
+});
 
